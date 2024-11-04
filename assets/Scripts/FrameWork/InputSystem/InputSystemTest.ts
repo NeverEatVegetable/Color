feab7d7fb0869e57033c810bb5b3d023c48d0ed4
Yuuu <1023653080@qq.com>
2024-11-05 00:20:50 +0800

    输入系统·测试版 实现掉帧移动

--- assets/Scripts/FrameWork/InputSystem/InputSystemTest.ts
+++ assets/Scripts/FrameWork/InputSystem/InputSystemTest.ts
@@ -1,49 +1,51 @@
-import { _decorator, Component, input, Input, EventKeyboard, KeyCode } from 'cc';
+import { _decorator, Component, input, Input, EventKeyboard, KeyCode, Vec2 } from 'cc';
 const { ccclass } = _decorator;
 
 @ccclass("InputSystemTest")
 export class InputSystemTest extends Component {
-    onLoad () {
+    onLoad() {
         input.on(Input.EventType.KEY_DOWN, this.onKeyDown, this);
+        input.on(Input.EventType.KEY_PRESSING, this.onKeyPressing, this);
         input.on(Input.EventType.KEY_UP, this.onKeyUp, this);
     }
 
-    onDestroy () {
+    onDestroy() {
         input.off(Input.EventType.KEY_DOWN, this.onKeyDown, this);
+        input.off(Input.EventType.KEY_PRESSING, this.onKeyPressing, this);
         input.off(Input.EventType.KEY_UP, this.onKeyUp, this);
     }
 
-    onKeyDown (event: EventKeyboard) {
-        switch(event.keyCode) {
+    onKeyDown(event: EventKeyboard) {
+
+    }
+
+    onKeyPressing(event: EventKeyboard) {
+        switch (event.keyCode) {
             case KeyCode.KEY_W:
-                console.log('Press a key W');
+                this.node.setPosition(this.node.position.x, this.node.position.y + 10);
                 break;
             case KeyCode.KEY_A:
-                console.log('Press a key A');
+                this.node.setPosition(this.node.position.x - 10, this.node.position.y);
                 break;
             case KeyCode.KEY_S:
-                console.log('Press a key S');
+                this.node.setPosition(this.node.position.x, this.node.position.y - 10);
                 break;
             case KeyCode.KEY_D:
-                console.log('Press a key D');
-            break;
+                this.node.setPosition(this.node.position.x + 10, this.node.position.y);
+                break;
         }
     }
 
-    onKeyUp (event: EventKeyboard) {
-        switch(event.keyCode) {
+    onKeyUp(event: EventKeyboard) {
+        switch (event.keyCode) {
             case KeyCode.KEY_W:
-                console.log('Release a key W');
                 break;
             case KeyCode.KEY_A:
-                console.log('Release a key A');
                 break;
             case KeyCode.KEY_S:
-                console.log('Release a key S');
                 break;
             case KeyCode.KEY_D:
-                console.log('Release a key D');
-            break;
+                break;
         }
     }
 }
