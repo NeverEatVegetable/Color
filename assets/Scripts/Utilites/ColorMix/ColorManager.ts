de2a7eab68b434d4ca3ca9f59a70866d513706e8
Disjoint3 <1463890764@qq.com>
2024-11-09 17:24:03 +0800

    放入混合表的json文件，颜色混合模块工作中
    颜色混合模块进度：新建文件夹。。。

---
+++ assets/Scripts/Utilites/ColorMix/ColorManager.ts
@@ -0,0 +1,15 @@
+import { _decorator, Component, Node, Sprite } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('ColorManager')
+export class ColorManager extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
+
