85bb8dbc90c9dbeeedf4d013632c730aabdd867e
Yuuu <1023653080@qq.com>
2024-12-22 16:03:52 +0800

    创建房间

---
+++ assets/Scripts/Views/room/views/RoomView.ts
@@ -0,0 +1,44 @@
+import { _decorator, Button, Component, Node, assetManager, instantiate, error } from 'cc'
+const { ccclass, property } = _decorator
+
+@ccclass('RoomView')
+export class RoomView extends Component {
+    private container: Node
+    private _tabs: Node
+    private btnClose: Button
+    buildUI() {
+        this._tabs = this.node.getChildByName('tabs')
+        this.btnClose = this.node.getChildByPath('leftTop/btnClose').getComponent(Button)
+        this.container = this.node.getChildByPath('container')
+    }
+
+    bindEvent() {
+        var content = this._tabs.getChildByPath('content')
+        for (let i = 0; i < content.children.length; i++) {
+            let btn = content.getChildByPath(`btn_${i+1}`).getComponent(Button)
+            btn.node.on('click', () => {
+                this._onClickTabBtn(i)
+            })
+        }
+        this.btnClose.node.on('click',this._onClickClose)
+    }
+
+    start() {
+        this.buildUI()
+        this.bindEvent()
+        this._onClickTabBtn(0)  // 默认打开第一个tab
+    }
+
+    private _onClickTabBtn(tabId) {
+        console.log('tabId = ', tabId)
+        for (let i = 0; i < this.container.children.length; i++) {
+            this.container.children[i].active = i == tabId
+        }
+    }
+
+    private _onClickClose() {
+        console.log('close')
+    }
+}
+
+
