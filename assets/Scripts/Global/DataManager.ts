786501c7da8173acb4969786723bc073d7f7a785
1Z1CC <2267379327@qq.com>
2025-01-01 19:30:13 +0800

    数据传输

--- assets/Scripts/Global/DataManager.ts
+++ assets/Scripts/Global/DataManager.ts
@@ -1,6 +1,6 @@
 import { AnimationClip, Prefab, Sprite, SpriteFrame } from "cc";
 import Singleton from "../Base/Singleton";
-import { EntityTypeEnum, FrameData, IActor, IActorMove, IState } from "../Common";
+import { EntityTypeEnum, IActor, IActorMove, IState } from "../Common";
 import { JoyStickManager } from "../UI/JoyStickManager";
 import { ActorMannager } from "../EntityAndActor/ActorMannager";
 import { ColorManager } from "../Utilites/ColorMix/ColorManager";
@@ -8,6 +8,8 @@ import { MyColor } from "../Utilites/ColorMix/MyColor";
 
 
 const ACTOR_SPEED=100
+const SEND_INTERVAL = 1000 / 30; //以30帧每秒的频率发送数据
+
 
 export default class DataManager extends Singleton implements BaseModel {
     static get Instance() {
@@ -60,6 +62,7 @@ export default class DataManager extends Singleton implements BaseModel {
         ],
     };
 
+    private lastSendTimestamp = 0; // 记录上一次发送数据的时间戳
 
     // 将actors数组转换为Map<string, Data>
     getPropToMap(): Map<string, any> {
@@ -72,6 +75,60 @@ export default class DataManager extends Singleton implements BaseModel {
         });
         return map;
     }
+
+    // 接收处理收到的数据
+    processFrameData(frameDataMap: Map<number, IActor>) {
+        frameDataMap.forEach((actorData, id) => {
+            const actor = this.state.actors.find((actor) => actor.id === id);
+            if (actor && actor.id !== this.getMyPlayerId()) {
+                actor.position = actorData.position;
+                actor.direction = actorData.direction;
+                actor.velocity = actorData.velocity;
+                actor.color = actorData.color;
+                actor.transparency = actorData.transparency;
+            }
+        });
+    }
+
+    // 发送数据的方法
+    private sendActorData() {
+        const dataMap = this.getPropToMap();
+        // 将Map序列化为可以发送的JSON字符串
+        const msg = JSON.stringify(Array.from(dataMap.values()));
+        // 发送消息给房间内全部玩家,所以type为0
+        RoomManager.instance.sendMsgToRoomMember(0, msg);
+        this.lastSendTimestamp = Date.now(); // 更新上一次发送数据的时间戳
+    }
+
+    // Update方法，每帧调用
+    update(dt: number) {
+        const now = Date.now();
+        if (now - this.lastSendTimestamp >= SEND_INTERVAL) {
+            this.sendActorData();
+        }
+    }
+
+    // 开启帧同步
+    startFrameSync() {
+        NotifyManager.instance.dispatch(GlobalNotify.RoomStartFrameSync);
+    }
+
+    // 关闭帧同步
+    stopFrameSync() {
+        NotifyManager.instance.dispatch(GlobalNotify.RoomStopFrameSync);
+    }
+
+    // 游戏进入时开启帧同步
+    onGameEnter() {
+        this.startFrameSync();
+    }
+
+    // 游戏退出时关闭帧同步
+    onGameExit() {
+        this.stopFrameSync();
+    }
+
+
 
     applyInput(input: IActorMove) {
         const {
@@ -106,18 +163,19 @@ export default class DataManager extends Singleton implements BaseModel {
         return this.myPlayerId;
     }
 
-    processFrameData(frameDataMap: Map<number, IActor>) {
-        frameDataMap.forEach((actorData, id) => {
-            const actor = this.state.actors.find((actor) => actor.id === id);
-            if (actor && actor.id !== this.getMyPlayerId()) {
-                actor.position = actorData.position;
-                actor.direction = actorData.direction;
-                actor.velocity = actorData.velocity;
-                actor.color = actorData.color;
-                actor.transparency = actorData.transparency;
-            }
+    constructor() {
+        super();
+        // 监听GameEnter和GameExit事件
+        NotifyManager.instance.addListener(GlobalNotify.GameEnter, this.onGameEnter);
+        NotifyManager.instance.addListener(GlobalNotify.Game0ver, this.onGameExit);
+        // 监听帧同步数据接收事件
+        NotifyManager.instance.addListener(GlobalNotify.RecvRoomFrameRecvMsg, (frameDataMap) => {
+            this.processFrameData(frameDataMap);
         });
     }
+
+
+
 }
 
 
