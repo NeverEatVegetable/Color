ff1dcc2f48fd66fcf7620e375facccce1e0e4a6e
Disjoint3 <1463890764@qq.com>
2025-01-01 13:07:08 +0800

    合流程、修改ui脚本

--- assets/Scripts/GameManager.ts
+++ assets/Scripts/GameManager.ts
@@ -11,29 +11,47 @@ export class GameManager extends Component {
     public viewPrefabs: any = {};   //key为prefab的名字，value为prefab
 
     private _startView:Node;
-    private _pauseView:Node;
+    private _pauseView: Node;
+    private _overView: Node;
 
     onLoad() {
         GameManager._Instance = this;
     }
 
     start() {
-        NotifyManager.instance.addListener(GlobalNotify.LOCAL_DATA_LOAD_SUCESS, () => {
-            this.InitOrder.bind(this);
-            this.InitScore.bind(this);
+        NotifyManager.instance.addListener(GlobalNotify.GameEnter, this.onGameStart.bind(this));
+        NotifyManager.instance.addListener(GlobalNotify.GameRestart, this.onGameStart.bind(this));
+        NotifyManager.instance.addListener(GlobalNotify.GamePause, this.onGamePause.bind(this));
+        NotifyManager.instance.addListener(GlobalNotify.GameResume, this.onGameResume.bind(this));
 
-            
-        });
-
-        input.on(Input.EventType.KEY_DOWN, this.onTouchStart, this);
     }
 
-    // #region 界面
+    onGameStart() {
+        this.InitOrder();
+        this.InitScore();
+
+        this.InitStartView();
+        this.InitPauseView();
+        this.InitOverView()
+    }
+
+    onGamePause() {
+        this._pauseView.active = true;
+    }
+
+    onGameResume() {
+        this._pauseView.active = false;
+    }
+
+
+    // #region ui界面
+
     InitStartView() {
         this._startView = instantiate(this.viewPrefabs["startView"]);
         this._startView.setParent(this.node);
         this._startView.setPosition(-640, -360);
-        this._startView.on(Node.EventType.TOUCH_END, this.onStartView,this);
+        this._startView.on(Node.EventType.TOUCH_END, this.onStartView, this);
+        this._startView.active = false;
     }
 
     onStartView() {
@@ -42,34 +60,22 @@ export class GameManager extends Component {
     }
 
     InitPauseView() {
-        this._startView = instantiate(this.viewPrefabs["startView"]);
-        this._startView.setParent(this.node);
-        this._startView.setPosition(-640, -360);
+        this._pauseView = instantiate(this.viewPrefabs["pauseView"]);
+        this._pauseView.setParent(this.node);
+        this._pauseView.setPosition(-640, -360);
+        this._pauseView.active = false;
         //绑定事件
     }
 
-    closePauseView() {
-        this._startView.active = false;
-    }
-
-    openPauseView() {
-        this._startView.active = true;
+    InitOverView() {
+        this._overView = instantiate(this.viewPrefabs["overView"]);
+        this._overView.setParent(this.node);
+        this._overView.setPosition(-640, -360);
+        this._overView.active = false;
     }
     // #endregion
 
-
-    onTouchStart(event: EventKeyboard) {
-        switch (event.keyCode) {
-            //提交订单，增加分数，生成新订单
-            case KeyCode.SPACE:
-                OrderManager._Instance.CheckOrder();
-                OrderManager._Instance.UpdateOrder();
-                OrderManager._Instance.OrderDataPrint();
-                break;
-            default:
-                break;
-        }
-    }
+    // #region 系统
 
     InitOrder() {
         OrderManager._Instance.orderNode = this.node.getChildByName("orderNode");
@@ -84,6 +90,8 @@ export class GameManager extends Component {
         ScoreManager._Instance.scoreNode = this.node.getChildByName("scoreNode");
         ScoreManager._Instance.Init();
     }
+
+    // #endregion
 }
 
 
